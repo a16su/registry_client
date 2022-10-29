489409bb450537a5172030402a1ed0031d3e5157
liyn-dev <1209515011@qq.com>
2022-10-30 02:17:21 +0800

    add Exception Type

--- registry_client/errors.py
+++ registry_client/errors.py
@@ -9,3 +9,52 @@ class ImageNotFoundError(Exception):
 class ImageManifestCheckError(Exception):
     def __init__(self):
         super(ImageManifestCheckError, self).__init__("image manifest check error")
+
+
+class ErrReferenceInvalidFormat(Exception):
+    def __init__(self):
+        super(ErrReferenceInvalidFormat, self).__init__("invalid reference format")
+
+
+class ErrTagInvalidFormat(Exception):
+    def __init__(self):
+        super(ErrTagInvalidFormat, self).__init__("invalid tag format")
+
+
+class ErrDigestInvalidFormat(Exception):
+    def __init__(self):
+        super(ErrDigestInvalidFormat, self).__init__("invalid digest format")
+
+
+class ErrNameContainsUppercase(Exception):
+    def __init__(self):
+        super(ErrNameContainsUppercase, self).__init__("repository name must be lowercase")
+
+
+class ErrNameEmpty(Exception):
+    def __init__(self):
+        super(ErrNameEmpty, self).__init__("repository name must have at least one component")
+
+
+class ErrNameTooLong(Exception):
+    def __init__(self):
+        super(ErrNameTooLong, self).__init__("repository name must not be more than 255 characters")
+
+
+class ErrNameNotCanonical(Exception):
+    def __init__(self):
+        super(ErrNameNotCanonical, self).__init__("repository name must be canonical")
+
+
+class ErrDigestInvalidLength(Exception):
+    def __init__(self):
+        super(ErrDigestInvalidLength, self).__init__("invalid checksum digest length")
+
+
+class ErrDigestUnsupported(Exception):
+    def __init__(self):
+        super(ErrDigestUnsupported, self).__init__("unsupported digest algorithm")
+
+
+if __name__ == "__main__":
+    raise ErrNameEmpty()
